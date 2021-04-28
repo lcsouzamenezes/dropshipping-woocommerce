b81904955b0b9aa966e04a26c81e83381576658d
Suraj Rathod <rathodsurajsp@gmail.com>
2021-04-28 20:31:28 +0530

    ask WPML addon direct install on setup

--- includes/knawat-required-plugins.php
+++ includes/knawat-required-plugins.php
@@ -62,6 +62,17 @@ function knawat_dropshipwc_register_required_plugins() {
 		);
 	}
 
+
+	if($knawat_dropshipwc->common->knawat_dropshipwc_is_wpml_activated()){
+		$plugins[] = array( 
+			'name'           => esc_html__( 'Knawat WooCommerce DropShipping WPML Addon', 'dropshipping-woocommerce' ),
+			'slug'           => 'dropshipping-woocommerce-wpml-addon',
+			'required'       => true,
+			'source'         => 'https://github.com/Knawat/dropshipping-woocommerce-wpml-addon/archive/master.zip',
+			'recommended_by' => 'knawat'
+		);
+	}
+
 	/*
 	 * Array of configuration settings. Amend each line as needed.
 	 *
